e76b8432d55be0cdbfe53b028acd90b9ece8fbf9
Rohit Batra <rohit@rohitbatra.in>
2018-07-21 20:12:53 +0530

    Changes to Applied Candidate List text api [wip]

--- wp-content/themes/jobseek/candidate_list.php
+++ wp-content/themes/jobseek/candidate_list.php
@@ -5,46 +5,16 @@
 get_header();
 global $resume_preview;
 include("database.php");
+$noApplicants = false;
 $jobID = $_GET['jobID'];
-
-$posts =  "SELECT * FROM applied_job_details WHERE job_ID = '{$jobID}'";
-$result = mysqli_query($conn, $posts);
+$sql =  "SELECT * FROM applied_job_details WHERE job_ID = '{$jobID}'";
+$result = mysqli_query($conn, $sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $applicantsid[$row['user_ID']] = $row;
+        $applicantIdArr[$row['user_ID']] = $row;
     }
 } else {
-   $noapplicants = 1;
-}
-var_dump($noapplicants);die();
-if(!$noapplicants) {
-
-	$applicantsidkeys = array_keys($applicantsid);
-	foreach ($applicantsidkeys as $key => $value) {
-        // QUERY TO FETCH THE USER POST
-        $posts  =  "SELECT ID FROM wp_posts WHERE post_author = ".$value;
-        $result = mysqli_query($conn, $posts);
-        if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-              $appid = $row;
-          }
-        }
-            // Get the Post meta
-        $applicantmeta[$value]['postdata'] = get_post($appid['ID'], ARRAY_A);
-        $applicantmeta[$value]['postmeta'] = get_post_meta($appid['ID']);
-        $applicantmeta[$value]['userdata'] = get_userdata($value);
-        $applicantmeta[$value]['meta'] = get_user_meta($value);
-        $applicantmeta[$value]['job_id'] = $jobID;
-
-        $posts  =  "SELECT approved_by_company FROM applied_job_details WHERE job_ID = '{$jobID}' AND user_ID = '{$value}' ";
-
-        $result = mysqli_query($conn, $posts);
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $applicantmeta[$value]['candidature_status'] = $row['approved_by_company'];
-            }
-        }
-	}
+   $noApplicants = true;
 }
 
 echo '
@@ -65,135 +35,158 @@ echo '
     }
 </script>';
 
-if($_GET['sendcall']) {
+if($noApplicants === false) {
 
-    if(array_key_exists('approve',$_GET)) {
+    if($_GET['sendcall'] == '1') {
 
-        $sendcall_id = $userId = $_GET['userID'];
+        if(array_key_exists('approve',$_GET)) {
 
-        $jobId = $_GET['jobID'];
+            $sendcall_id = $userId = $_GET['userID'];
 
-        $posts  =  "SELECT approved_by_company FROM applied_job_details WHERE job_ID = '{$jobId}' AND user_ID = '{$userId}'";
+            $jobId = $_GET['jobID'];
 
-        $result = mysqli_query($conn, $posts);
-
-        if ($result->num_rows > 0){
-            while($row = $result->fetch_assoc()) {
-                $apr = $row['approved_by_company'];
-            }
-        }
-
-        if($apr !== '1' && $flg === false) {
-
-            $posts = "UPDATE applied_job_details SET approved_by_company = '1' WHERE job_ID = '{$jobId}' AND user_ID = '{$userId}'";
+            $posts  =  "SELECT approved_by_company FROM applied_job_details WHERE job_ID = '{$jobId}' AND user_ID = '{$userId}'";
 
             $result = mysqli_query($conn, $posts);
 
-            //-----------------------------------------
-            // Send Call Letter Functionality
-            //-----------------------------------------
-
-            if(strlen($applicantmeta[$sendcall_id]['meta']['phone_no'][0]) == 0) {
-                $candidate_phone_number = '9768230192';
-
-            } else if(strlen($applicantmeta[$sendcall_id]['meta']['phone_no'][0]) > 10) {
-                $candidate_phone_number = substr((int)$applicantmeta[$sendcall_id]['meta']['phone_no'][0],2,10);
-            } else {
-                $candidate_phone_number = $applicantmeta[$sendcall_id]['meta']['phone_no'][0];
+            if ($result->num_rows > 0){
+                while($row = $result->fetch_assoc()) {
+                    $apr = $row['approved_by_company'];
+                }
             }
 
-            $candidate_name  =  $applicantmeta[$sendcall_id]['meta']['first_name'][0] ." ". $applicantmeta[$sendcall_id]['meta']['last_name'][0];
+            if($apr !== '1') {
 
-            if(!$candidate_phone_number) {
+                $posts = "UPDATE applied_job_details SET approved_by_company = '1' WHERE job_ID = '{$jobId}' AND user_ID = '{$userId}'";
 
-                echo "<p class='error center'>Phone Number is not available for {$jobmeta['_candidate_name'][0]}.</p>";
+                $result = mysqli_query($conn, $posts);
 
-            } else {
+                //-----------------------------------------
+                // Send Call Letter Functionality
+                //-----------------------------------------
 
-                // We got the phone number. send message.
-                // Get the job meta first, we need contact person details.
+                if(strlen($applicantMetaArr[$sendcall_id]['meta']['phone_no'][0]) == 0) {
+                    $candidate_phone_number = '9768230192';
 
-                $jobmeta = get_post_meta($jobID);
-
-                $apiKey = "fIGPfvyRREQ-AhC2MBBByyTkOBrtdr6yGarPJlWxFQ";
-
-                // Config variables. Consult http://api.textlocal.in/docs for more info.
-                $test = "0";
-                // Data for text message. This is the text message data.
-                $sender = "SEZPLS"; // This is who the message appears to be from.
-                $numbers = "91".$candidate_phone_number; // A single number or a comma-seperated list of numbers
-
-                // Check for the Company Details too
-                if(strlen($jobmeta['_contact_person'][0]) == 0 || strlen($jobmeta['_contact_num'][0]) == 0) {
-
-                    // SQL to Find company contact Info
-                    $cmpMeta  =  "SELECT wp_um.meta_key, wp_um.meta_value FROM wp_usermeta AS wp_um WHERE wp_um.user_id = (SELECT post_author FROM wp_posts WHERE ID = '{$jobId}')";
-
-                    $result = mysqli_query($conn, $cmpMeta);
-
-                    while($row = $result->fetch_assoc()) {
-
-                        if($row['meta_key'] == '_contact_person'){
-                            $contactPerson = substr($row['meta_value'],0,20);
-                        }elseif ($row['meta_key'] == '_contact_num'){
-                            $contactNumber = substr($row['meta_value'],0,17);
-                        }
-                    }
-
+                } else if(strlen($applicantMetaArr[$sendcall_id]['meta']['phone_no'][0]) > 10) {
+                    $candidate_phone_number = substr((int)$applicantMetaArr[$sendcall_id]['meta']['phone_no'][0],2,10);
                 } else {
-                    $contactPerson = substr($jobmeta['_contact_person'][0],0,20);
-                    $contactNumber = substr($jobmeta['_contact_num'][0],0,17);
+                    $candidate_phone_number = $applicantMetaArr[$sendcall_id]['meta']['phone_no'][0];
                 }
 
-                $message = "Hello ".substr($candidate_name,0,13).", you are selected for interview at ".substr($jobmeta['_company_name'][0],0,23).". Please Contact: ".$contactPerson.", Phone: ".$contactNumber." - SEZPLUS Team";
+                $candidate_name  =  $applicantMetaArr[$sendcall_id]['meta']['first_name'][0] ." ". $applicantMetaArr[$sendcall_id]['meta']['last_name'][0];
 
-                // 612 chars or less
-                $message = urlencode($message);
+                if(!$candidate_phone_number) {
 
-                $data = "apiKey=".$apiKey."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+                    echo "<p class='error center'>Phone Number is not available for {$jobmeta['_candidate_name'][0]}.</p>";
 
-                $ch = curl_init('http://api.textlocal.in/send/?');
+                } else {
 
-                curl_setopt($ch, CURLOPT_POST, true);
+                    // We got the phone number. send message.
+                    // Get the job meta first, we need contact person details.
 
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                    $jobmeta = get_post_meta($jobID);
 
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    $apiKey = "fIGPfvyRREQ-AhC2MBBByyTkOBrtdr6yGarPJlWxFQ";
 
-                $result = curl_exec($ch); // This is the result from the API
+                    // Config variables. Consult http://api.textlocal.in/docs for more info.
+                    $test = "0";
+                    // Data for text message. This is the text message data.
+                    $sender = "SEZPLS"; // This is who the message appears to be from.
+                    $numbers = "91".$candidate_phone_number; // A single number or a comma-seperated list of numbers
 
-                curl_close($ch);
+                    // Check for the Company Details too
+                    if(strlen($jobmeta['_contact_person'][0]) == 0 || strlen($jobmeta['_contact_num'][0]) == 0) {
 
-                //-------------------------------------------------------
+                        // SQL to Find company contact Info
+                        $cmpMeta  =  "SELECT wp_um.meta_key, wp_um.meta_value FROM wp_usermeta AS wp_um WHERE wp_um.user_id = (SELECT post_author FROM wp_posts WHERE ID = '{$jobId}')";
+
+                        $result = mysqli_query($conn, $cmpMeta);
+
+                        while($row = $result->fetch_assoc()) {
+
+                            if($row['meta_key'] == '_contact_person'){
+                                $contactPerson = substr($row['meta_value'],0,20);
+                            }elseif ($row['meta_key'] == '_contact_num'){
+                                $contactNumber = substr($row['meta_value'],0,17);
+                            }
+                        }
+
+                    } else {
+                        $contactPerson = substr($jobmeta['_contact_person'][0],0,20);
+                        $contactNumber = substr($jobmeta['_contact_num'][0],0,17);
+                    }
+
+                    $message = "Hello ".substr($candidate_name,0,13).", you are selected for interview at ".substr($jobmeta['_company_name'][0],0,23).". Please Contact: ".$contactPerson.", Phone: ".$contactNumber." - SEZPLUS Team";
+
+                    // 612 chars or less
+                    $message = urlencode($message);
+
+                    $data = "apiKey=".$apiKey."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+
+                    $ch = curl_init('http://api.textlocal.in/send/?');
+
+                    curl_setopt($ch, CURLOPT_POST, true);
+
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+                    $result = curl_exec($ch); // This is the result from the API
+
+                    curl_close($ch);
+
+                    //-------------------------------------------------------
+                }
             }
-            // Redirect to the Applied List Page
-            echo '
-                <script type="text/javascript">
-                    window.location = "//sezplus.com/jobs/applied-candidate-list/?jobID='.$jobId.'&userID='.$userId.'";
-                </script>';
-        }
-    }
-}
 
-if($applicantmeta){
-  echo '<section id="content">
-           <div class="container">
-             <div class="table-responsive">
-                 <table class="job-manager-jobs table table-striped">
-                   <thead>
-                   <tr>
-                     <th class="job_title">#</th>
-                     <th class="date">Candidate Name</th>
-                     <th class="date">Location</th>
-                     <th class="date">Action</th>
-                   </tr>
-                   </thead>
-                   <tbody>
-                 ';
-                 $i=0;
-	foreach ($applicantmeta as $key => $value) {
-      $i++;
-			echo "
+            $applicantsIdArrKeys = array_keys($applicantIdArr);
+
+            foreach ($applicantsIdArrKeys as $key => $value) {
+
+                // QUERY TO FETCH THE USER POST
+                $userSql  =  "SELECT ID FROM wp_posts WHERE post_author = '{$value}' LIMIT 1";
+                $result = mysqli_query($conn, $userSql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        $applicantId = $row['ID'];
+                    }
+                }
+                // Get the Post meta
+                $applicantMetaArr[$value]['postdata'] = get_post($applicantId, ARRAY_A);
+                $applicantMetaArr[$value]['postmeta'] = get_post_meta($applicantId);
+                $applicantMetaArr[$value]['userdata'] = get_userdata($value);
+                $applicantMetaArr[$value]['meta'] = get_user_meta($value);
+                $applicantMetaArr[$value]['job_id'] = $jobID;
+
+                $posts  =  "SELECT approved_by_company FROM applied_job_details WHERE job_ID = '{$jobID}' AND user_ID = '{$value}' ";
+
+                $result = mysqli_query($conn, $posts);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        $applicantMetaArr[$value]['candidature_status'] = $row['approved_by_company'];
+                    }
+                }
+            }
+
+            echo '<section id="content">
+                   <div class="container">
+                     <div class="table-responsive">
+                         <table class="job-manager-jobs table table-striped">
+                           <thead>
+                           <tr>
+                             <th class="job_title">#</th>
+                             <th class="date">Candidate Name</th>
+                             <th class="date">Location</th>
+                             <th class="date">Action</th>
+                           </tr>
+                           </thead>
+                           <tbody>
+                         ';
+            $i=0;
+            foreach ($applicantMetaArr as $key => $value) {
+                $i++;
+                echo "
               <tr>
                 <td>{$i}</td>
                 <td><a href='{$value['postdata']['guid']}?referrer=c_list' target='_blank'>{$value['postmeta']['_candidate_name'][0]}</a></td>
@@ -205,15 +198,20 @@ if($applicantmeta){
                     $path = pathinfo($_SERVER['REQUEST_URI']);
                     echo '<td><a href="//sezplus.com/jobs/applied-candidate-list?jobID='.$value['job_id'].'&userID='.$key.'&approve=1&sendcall=1"><button class="vc_general vc_btn3 vc_btn3-size-sm vc_btn3-shape-rounded vc_btn3-style-outline vc_btn3-icon-left vc_btn3-color-mulled-wine"><i class="vc_btn3-icon fa fa-envelope"></i> Send A Call Letter</button></a></td>';
                 }
-            echo " </tr>";
+                echo " </tr>";
 
 
-	}
-    echo '        </tbody>
+            }
+            echo '        </tbody>
                 </table>
               </div>
             </div>
           </section>';
+        }
+    }
+
+}else{
+    echo '<h4> No Applicant(s) Yet!</h5><br/>';
 }
 
 get_footer();
